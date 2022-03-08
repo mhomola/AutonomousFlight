cb9298f5c433e165017e02e8cbf0944730a7a7c8
Nathaniel <nps.dahl@gmail.com>
2022-03-08 16:22:25 +0100

    Added RobotType enum, added Config Struct. Removed self.ob.

--- DynamicWindowNavigation/main.cpp
+++ DynamicWindowNavigation/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <tuple>
+#include <math.h>
 
 tuple<vect, vect> dwa_control(vect x, Config config, vect goal, objectlist ob){
     //Top level control function
@@ -15,8 +16,63 @@ tuple<vect, vect> dwa_control(vect x, Config config, vect goal, objectlist ob){
     return  {u, trajectory};
 }
 
-auto class Config:
-//TODO Nathaniel
+enum RobotType {circle, rectangle};
+
+struct Config{
+    //TODO Nathaniel
+    //simulation parameter class
+    //Tune stoff here
+
+    using enum RobotType;
+
+    Config() {
+        // robot parameterint
+        //TODO Tune parameters. Currently all still on default
+        //TODO implement pi
+        float max_speed = 1.0;  // [m/s]
+        float min_speed = -0.5;  // [m/s]
+        float max_yaw_rate = 40.0 * M_PI / 180.0;  // [rad/s]
+        float max_accel = 0.2;  // [m/ss]
+        float max_delta_yaw_rate = 40.0 * M_PI / 180.0;  // [rad/ss]
+        float v_resolution = 0.01;  // [m/s]
+        float yaw_rate_resolution = 0.1 * M_PI / 180.0;  // [rad/s]
+        float dt = 0.1;  // [s] Time tick for motion prediction
+        float predict_time = 3.0;  // [s]
+        float to_goal_cost_gain = 0.15;
+        float speed_cost_gain = 1.0;
+        float obstacle_cost_gain = 1.0;
+        float robot_stuck_flag_cons = 0.001;  // constant to prevent robot stucked
+        RobotType robot_type = circle;
+
+        // if robot_type == RobotType.circle
+        // Also used to check if goal is reached in both types
+        float robot_radius = 1.0;  // [m] for collision check
+
+        // if robot_type == RobotType.rectangle
+        float robot_width = 0.5;  // [m] for collision check
+        float robot_length = 1.2;  // [m] for collision check
+        // obstacles [x(m) y(m), ....]
+        /*
+        Currently removed this. We should implement the object list differently then the python script
+        self.ob = np.array([[-1, -1],
+                            [0, 2],
+                            [4.0, 2.0],
+                            [5.0, 4.0],
+                            [5.0, 5.0],
+                            [5.0, 6.0],
+                            [5.0, 9.0],
+                            [8.0, 9.0],
+                            [7.0, 9.0],
+                            [8.0, 10.0],
+                            [9.0, 11.0],
+                            [12.0, 13.0],
+                            [12.0, 12.0],
+                            [15.0, 15.0],
+                            [13.0, 13.0]
+                            ]);
+        */
+        }
+};
 
 def motion(x, u, dt):
     //TODO figure out if this is just simulating the real world. Do we need to implement something here?

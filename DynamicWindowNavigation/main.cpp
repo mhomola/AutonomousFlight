1eee9f2be2f2531393bb6aff9c554eb0432bc3a1
GeorgStrunck <georg.strunck@t-online.de>
2022-03-10 18:28:46 +0100

    Fix some wrong dimensions in predict_trajectory
    Fix some wrong dimensions in predict_trajectory

--- DynamicWindowNavigation/main.cpp
+++ DynamicWindowNavigation/main.cpp
@@ -108,11 +108,11 @@ Matrix<float, 31, 5> predict_trajectory(Matrix<float, 1, 5> x_init, float v, flo
 		// predict trajectory with an input
 
 		Matrix<float, 1, 5>  x = {x_init};
-		MatrixXf  trajectory= {x};							//starts with just x, but then vstacks them up to 31 times
+		Matrix<float, 31, 5>  trajectory= {x};							//starts with just x, but then vstacks them up to 31 times
 		float time = 0;
 		while (time <= int config.predict_time){
 			Matrix<float, 1, 5> x = motion(x, [v, y], float config.dt);
-			MatrixXf trajectory = vstack(trajectory, x) 		//not sure whether vstackworks this way
+			Matrix<float, 31, 5> trajectory = vstack(trajectory, x) 		//not sure whether vstackworks this way
 		    float time += config.dt;
 		}
 		return trajectory;
@@ -121,7 +121,7 @@ Matrix<float, 31, 5> predict_trajectory(Matrix<float, 1, 5> x_init, float v, flo
 def calc_control_and_trajectory(x, dw, config, goal, ob):
 //TODO Nathaniel
 
-def calc_obstacle_cost(trajectory, ob, config):
+float calc_obstacle_cost(trajectory, ob, config):
 //TODO Georg
 		//calc obstacle cost inf: collision
 
@@ -158,41 +158,7 @@ def calc_obstacle_cost(trajectory, ob, config):
 		return 1.0 / min_r;		// OK
 
 }
-//TODO Georg
 
-/*
- * """
-    calc obstacle cost inf: collision
-    """
-    ox = ob[:, 0]
-    oy = ob[:, 1]
-    dx = trajectory[:, 0] - ox[:, None]
-    dy = trajectory[:, 1] - oy[:, None]
-    r = np.hypot(dx, dy)
-
-    if config.robot_type == RobotType.rectangle:
-        yaw = trajectory[:, 2]
-        rot = np.array([[np.cos(yaw), -np.sin(yaw)], [np.sin(yaw), np.cos(yaw)]])
-        rot = np.transpose(rot, [2, 0, 1])
-        local_ob = ob[:, None] - trajectory[:, 0:2]
-        local_ob = local_ob.reshape(-1, local_ob.shape[-1])
-        local_ob = np.array([local_ob @ x for x in rot])
-        local_ob = local_ob.reshape(-1, local_ob.shape[-1])
-        upper_check = local_ob[:, 0] <= config.robot_length / 2
-        right_check = local_ob[:, 1] <= config.robot_width / 2
-        bottom_check = local_ob[:, 0] >= -config.robot_length / 2
-        left_check = local_ob[:, 1] >= -config.robot_width / 2
-        if (np.logical_and(np.logical_and(upper_check, right_check),
-                           np.logical_and(bottom_check, left_check))).any():
-            return float("Inf")
-    elif config.robot_type == RobotType.circle:
-        if np.array(r <= config.robot_radius).any():
-            return float("Inf")
-
-    min_r = np.min(r)
-    return 1.0 / min_r  # OK
- *
- */
 def calc_to_goal_cost(trajectory, goal):
 //TODO Thijs
 

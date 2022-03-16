233c9a045a2b178958b438908248896f45714b75
mhomola <72628012+mhomola@users.noreply.github.com>
2022-03-16 11:29:46 +0100

    Path fixed

--- CV_scripts/FloorDetection.py
+++ CV_scripts/FloorDetection.py
@@ -12,22 +12,20 @@ import utils
 
 #data_folder = r'Data\cyberzoo_poles'
 #image_folder = str(data_folder + r'\20190121-135009' )
-image_folder = r'C:/Users/mhomo/OneDrive/Dokumenty/University/GitHub/AutonomousFlight/CV_scripts/Data/cyberzoo_poles/20190121-135009'
+image_folder = os.getcwd() + '/../Data/cyberzoo_poles/20190121-135009';
 
-
-def filter_color(im, y_low=50, y_high=200, u_low=120, u_high=130, v_low=120, v_high=130, resize_factor=1):
+def filter_color(im, y_low=50, y_high=200, u_low=120, u_high=130, v_low=120, v_high=130, resize_factor=3):
     #im = icv2.imread(image_name)
     im = cv2.resize(im, (int(im.shape[1] / resize_factor), int(im.shape[0] / resize_factor)))
     YUV = cv2.cvtColor(im, cv2.COLOR_BGR2YUV)
     Filtered = np.zeros([YUV.shape[0], YUV.shape[1]])
+    print(YUV.shape[0]/3)
     for y in range(YUV.shape[0]):
         for x in range(YUV.shape[1]):
             if (y_low <= YUV[y, x, 0] <= y_high and
                     u_low <= YUV[y, x, 1] <= u_high and
                     v_low <= YUV[y, x, 2] <= v_high):
                 Filtered[y, x] = 1
-
-    print('got here')
 
     plt.figure()
     RGB = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)

babc8a81d6bce5a6306fe371abee4bdc474081da
Nathaniel <nps.dahl@gmail.com>
2022-03-19 11:23:25 +0100

    added working files for DWN. Maybe missing some bits still but it compiles

---
+++ sw/airborne/modules/dynamic_window_navigation/dynamic_window.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) Roland Meertens
+ *
+ * This file is part of paparazzi
+ *
+ */
+/**
+ * @file "modules/orange_avoider/orange_avoider.h"
+ * @author Roland Meertens
+ * Example on how to use the colours detected to avoid orange pole in the cyberzoo
+ */
+
+#ifndef DYNAMIC_WINDOW_H
+#define DYNAMIC_WINDOW_H
+
+// settings
+extern float oa_color_count_frac;
+
+// functions
+extern void orange_avoider_init(void);
+extern void orange_avoider_periodic(void);
+
+//TODO CHANGE VARIABLE NAMES
+
+#endif
+

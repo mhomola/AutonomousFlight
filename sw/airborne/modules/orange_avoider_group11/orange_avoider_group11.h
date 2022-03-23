6b1dd20a6ad3d165e089ac6d62094b8cd8bbad66
Dries <dries.borstlap@gmail.com>
2022-03-23 16:11:26 +0100

    orange_avoider_second_rewrite

---
+++ sw/airborne/modules/orange_avoider_group11/orange_avoider_group11.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) Roland Meertens
+ *
+ * This file is part of paparazzi
+ *
+ */
+/**
+ * @file "modules/orange_avoider/orange_avoider.h"
+ * @author Roland Meertens
+ * Example on how to use the colours detected to avoid orange pole in the cyberzoo
+ */
+
+#ifndef ORANGE_AVOIDER_H
+#define ORANGE_AVOIDER_H
+
+// settings
+extern float oa_color_count_frac;
+
+// functions
+extern void orange_avoider_init(void);
+extern void orange_avoider_periodic(void);
+
+#endif
+

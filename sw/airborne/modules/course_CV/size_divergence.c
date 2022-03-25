e36660949a947d4713678edca2d2e10699f09a7b
Nathaniel <nps.dahl@gmail.com>
2022-03-25 09:48:49 +0100

    changed return to flow_r+flow_r

--- sw/airborne/modules/course_CV/size_divergence.c
+++ sw/airborne/modules/course_CV/size_divergence.c
@@ -99,7 +99,7 @@ float get_heading_command(struct flow_t *vectors, int count, int img_size, int s
   // compute simple yaw/heading change command
   yaw_command = (flow_l - flow_r)/(flow_l + flow_r);
   if (flow_l + flow_r > NEG2THRESHOLD) {
-    return -2.0;
+    return flow_l+flow_r;
   } else if (isnan(yaw_command) || (fabs(yaw_command) < YAW_THRESHOLD ) || flow_l < 5.0 || flow_r < 5.0) {
     return 0.0;
   } 
